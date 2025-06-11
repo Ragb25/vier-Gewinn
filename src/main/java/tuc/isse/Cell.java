64bb0ff66b9399f99b6af23cdbab20d127411d7c
Tiegam Njoya Franck Wilfried <ft81@tu-clausthal.de>
2025-06-12 00:54:22 +0200

    javadoc Cell und token

--- src/main/java/tuc/isse/Cell.java
+++ src/main/java/tuc/isse/Cell.java
@@ -1,21 +1,31 @@
 package tuc.isse;
 
+/**
+ *email:ragb25@tu-clausthal.de
+ * email: Ft81@tu-clausthal.de
+ *@autor1 Ramane Gbatkom Mouliom Abdel
+ *@author2 TIEGAM NJOYA FRANCK WILFRIED
+ */
+
+// class Cell
 public class Cell extends GameObjekt {
     private Token token;
 
+    //instance of class
     public Cell(){
         this.token = null;
 
     }
 
+
+    //nimmt der Token
     public Token getToken() {
         return token;
     }
 
-    public void setToken(Token token) {
-        this.token = token;
-    }
 
+
+   //die methode placetoken erlaubt es, einmalig ein Token zu legen, wenn die zelle leer ist
     public boolean placetoken(Token t){
         if (this.token == null){
             this.token = t;
@@ -24,13 +34,19 @@ public class Cell extends GameObjekt {
         return false;
     }
 
+
+    // die methode überpruft, ob eine zelle leer ist
     public boolean isleer(){
 
         return this.token == null;
     }
 
 
-
+    /*diese methode hier gibt eine darstellung der zelle
+     * "( )" leer
+     * "(x)" token RED
+     * "(O)"token BLUE
+     */
     @Override
     public String toString() {
         if (token == null){
--- src/main/java/tuc/isse/Token.java
+++ src/main/java/tuc/isse/Token.java
@@ -1,19 +1,32 @@
 package tuc.isse;
 
+/**
+* email:ragb25@tu-clausthal.de
+* email: Ft81@tu-clausthal.de
+*@autor1 Ramane Gbatkom Mouliom Abdel
+*@author2 TIEGAM NJOYA FRANCK WILFRIED
+*/
+
+//class Token
 public class Token extends GameObjekt {
     private Color color;
 
-
+    //instance of class
     public Token(Color color) {
         this.color = color;
     }
 
-
+    // nimmt die farbe
     public Color getColor() {
 
         return this.color;
     }
 
+
+
+    /*return eine farbe des Tokens
+     *X = RED, O = BLUE
+     */
     @Override
     public String toString() {
         return (color == Color.RED) ? "X" : "O";
