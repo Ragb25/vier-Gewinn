8e70b57a5b7fe1bb0d01c5c791254bcb4ef87f8b
Ramane Gbatkom Mouliom Abdel <ramane.gbatkom.mouliom.abdel@tu-clausthal.de>
2025-06-11 12:32:27 +0200

    Update von Bord mit Änderungen von wichtige Methoden

--- src/main/java/tuc/isse/Bord.java
+++ src/main/java/tuc/isse/Bord.java
@@ -1,14 +1,19 @@
 package tuc.isse;
 
+import javax.swing.*;
+
 public class Bord extends GameObjekt{
 
-    private String [][] gameBord  = new String[6][7];
-    private token token;
+    private Cell[][] gameBord  = new Cell[6][7]; // Das Spielfeld
+    private Token token;
 
-    public Bord(String [][] gameBord, token token){
+    public Bord(Cell [][] gameBord, Token token){
         this.gameBord = gameBord;
         this.token = token;
     }
+    /*
+    Jetzt füllen wier unser Spielfeld mit den Cell.
+     */
     public Bord(String [] GameBord){
         for(int i=0; i<6; i++){
             for(int j=0; j<7; j++){
@@ -19,13 +24,54 @@ public class Bord extends GameObjekt{
 
     }
 
-    public String[][] getGameBord() {
+    /*
+    Getter und setter für das gamebord
+     */
+    public Cell[][] getGameBord() {
         return gameBord;
     }
 
-    public void setGameBord(String[][] gameBord) {
+    public void setGameBord(Cell[][] gameBord) {
         this.gameBord = gameBord;
     }
+    /*
+    Die Methode canDrop. Die wird überprüfen, ob eine Cell bereits besetzt ist und den entsprechende Entscheidung treffen.
+     */
+    public boolean canDrop(int columnsIndex){
+        if(columnsIndex<=0 || columnsIndex>7){
+            return false;
+        } else return gameBord[0][columnsIndex].isleer();
+    }
 
+    /*
+    Die Methode dropToken prüft zuerst, ob eine Spalte besetzt werden kann. Wenn möglich stellt sie ein Token in der unterste cell
+     */
+    public boolean dropToken(color color, int columnsIndex){
+        if (!canDrop(columnsIndex)){
+            return false;
+        }
+        for(int i=5; i>=0; i--){
+            if(gameBord[i][columnsIndex].isleer()){
+                gameBord[i][columnsIndex].setToken(new Token(color));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*
+    Die Methode toString gibt einfach das ganze Spielfeld als String zurück
+     */
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        for (int r = 0; r < 6; r++) {
+            for (int c = 0; c < 7; c++) {
+                sb.append(gameBord[r][c].toString());
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 
 }
