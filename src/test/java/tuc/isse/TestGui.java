d2ac243b61cd5b6eb81d58b5ec0be49182ad1bc2
Ramane Gbatkom Mouliom Abdel <ramane.gbatkom.mouliom.abdel@tu-clausthal.de>
2025-07-17 12:58:30 +0200

    TestBoard

--- src/test/java/tuc/isse/TestGui.java
+++ src/test/java/tuc/isse/TestGui.java
@@ -10,7 +10,7 @@ public class TestGui {
         // Erstelle eine Instanz von ObservableBord anstelle von Bord
         ObservableBord bord = new ObservableBord(); // Verwende ObservableBord
         VierGewinntFrame frame = new VierGewinntFrame(bord);
-        bord.addObserver( frame);  // Füge den Observer hinzu
+        bord.addObserver((BordObserver) frame);  // Füge den Observer hinzu
 
         // Simuliere einen Zug
         Token token = bord.getToken(Color.RED); // Beispiel: Token rot
